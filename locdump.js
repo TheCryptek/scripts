274acad6b07717c37003b9404b6d6e6e24bd5a40
TheCryptek <thecryptek@outlook.com>
2018-01-10 00:00:24 -0500

    Made a few changes/improvements after adding the actual locdump list.

--- locdump.js
+++ locdump.js
@@ -4,8 +4,16 @@ function(context, args) {
 	 if( found == null ) {
 		  return "\n`D/!\\` `FMaintenance Mode` `D/!\\`"
 	 }
-	 
 	 return [
+		" ",
+		#db.f({loc:{$exists:true}}).sort({sec:1}).array().map(o=>{
+		var loc=o.loc
+		var sec=#fs.scripts.get_level({name:loc})
+		sec=['`dNull','`DLow','`FMid','`JHigh','`LFull'][sec]+'Sec`'
+		return loc+' | '+sec
+		}).join('\n'),
+		" ",
+		"Sec levels are checked once a day",
 		" ",
 		"This loc dump was made with help from `DDTR` and `Ddo`"
 	 ]
